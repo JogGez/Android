034c3d96f51d9d80ccaa6e8812382180c57b7996
Julie Dittmann Weimar Andersen <smukkejul@live.dk>
2020-05-02 20:05:17 +0200

    added UI

---
+++ WeatherApp/app/src/main/java/com/androdocs/weatherapp/AddCityActivity.java
@@ -0,0 +1,14 @@
+package com.androdocs.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_city);
+    }
+}
---
+++ WeatherApp/app/src/main/java/com/androdocs/weatherapp/ManageCitiesActivity.java
@@ -0,0 +1,14 @@
+package com.androdocs.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageCitiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_cities);
+    }
+}
