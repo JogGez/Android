f8ecc31337d56a4058c9b4f40cf2409b19010f6e
Simon <simon-nj@hotmail.com>
2020-05-24 12:58:17 +0200

    Change ApiInterface to Interface

--- WeatherApp/app/src/main/java/com/androdocs/weatherapp/API/WeatherApiInterface.java
+++ WeatherApp/app/src/main/java/com/androdocs/weatherapp/API/WeatherApiInterface.java
@@ -1,12 +1,18 @@
 package com.androdocs.weatherapp.API;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-public class WeatherApiInterface {
+public interface WeatherApiInterface {
 
-    //@GET("forecast")
-    //Call<> (@Query("q") String city, @Query("APPID")String key, @Query("units") String units , @Query("cnt") int days);
+    @GET("forecast")
+    Call<ResponseBody> queryWeatherData(
+            @Query("q") String city,
+            @Query("APPID")String key,
+            @Query("units") String units,
+            @Query("cnt") int days
+            );
 
 }
